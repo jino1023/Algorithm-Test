1373cf2c9941cfffecbc372cada4311afda05581
Jinoh <pjh92147154@gmail.com>
2023-01-03 11:32:04 +0900

    230103

---
+++ baekjoon/3003.java
@@ -0,0 +1,28 @@
+// 3003 킹, 퀸, 룩, 비숍, 나이트, 폰
+import java.util.Scanner;
+class Main {  
+  public static void main(String args[]) { 
+    Scanner in = new Scanner(System.in);
+
+    int k = 1;
+    int q = 1;
+    int r = 2;
+    int b = 2;
+    int kn = 2;
+    int p = 8;
+
+    k = k - in.nextInt();
+    q = q - in.nextInt();
+    r = r - in.nextInt();
+    b = b - in.nextInt();
+    kn = kn - in.nextInt();
+    p = p - in.nextInt();
+    
+    System.out.print(k+" ");
+    System.out.print(q+" ");
+    System.out.print(r+" ");
+    System.out.print(b+" ");
+    System.out.print(kn+" ");
+    System.out.print(p);
+  } 
+}
