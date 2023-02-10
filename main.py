de738ad434bd565fab005ade2fce3e97fc2216d2
Jinoh <45726824+jino1023@users.noreply.github.com>
2023-02-10 06:54:17 +0000

    230210

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-# 영어 끝말잇기
-# https://school.programmers.co.kr/learn/courses/30/lessons/12981
-def solution(n, words):
-    used_words=[]
-    for i in range(len(words)):
-        if i == 0:
-            used_words.append(words[i])
-        else:
-            if words[i] in used_words or words[i][0] != words[i-1][-1]:
-                return [(i%n)+1,(i//n)+1]
-            used_words.append(words[i])
-    return [0, 0]
+# 점프와 순간이동
+# https://school.programmers.co.kr/learn/courses/30/lessons/12980
+def solution(n):
+  ans = 0
+  while n > 0:
+    if n % 2 == 0:
+      n = n // 2
+    else:
+      n -= 1
+      ans += 1
+  return ans
